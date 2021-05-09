e13b8f135a95a3e55326bba79420504a767f4bf5
Maxime DONNE <donne.maxime@gmail.com>
2021-05-09 15:57:47 +0200

    Creation du fichier categorie gestion de la creation d un csv pour la categorie poesie pas de prise en compte de plusieurs pages

---
+++ categorie.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup as BS
+import csv
+import re
+
+urlbase ='http://books.toscrape.com'
+urlcatalogue ='http://books.toscrape.com/catalogue'
+url ='http://books.toscrape.com/catalogue/category/books/poetry_23/index.html'
+category ='poetry'
+response = requests.get(url)
+fichcsv = 'poetry.csv'
+
+def livre(url,category,fichiercsv):
+    urlbase = 'http://books.toscrape.com'
+    response = requests.get(url)
+    if response.ok:
+        soup = BS(response.content, features="html.parser")
+        title = soup.find('title').text
+        UPC = soup.find('th', string='UPC').next_sibling.text
+        PriceWithTax = soup.find('th', string='Price (incl. tax)').next_sibling.text
+        PriceExcludingTax = soup.find('th', string='Price (excl. tax)').next_sibling.text
+        numberAvailable = soup.find('th', string='Availability').next_sibling.next_sibling.text
+        numberAvailable = re.findall("\d+", numberAvailable)
+        numberAvailable = numberAvailable[0]
+        productDescription = soup.find('div', id="product_description").next_sibling.next_sibling.text
+        nbReview = soup.find('th', string='Number of reviews').next_sibling.next_sibling.text
+        imageUrl = soup.find('div', id="product_gallery").find('img')['src']
+        imageUrl = imageUrl[6:]
+        imageUrl = (urlbase, imageUrl)
+        imageUrl = '/'.join(imageUrl)
+        with open(fichiercsv, 'a', newline='') as fichiercsv:
+            writer = csv.writer(fichiercsv)
+            writer.writerow([url, UPC, title, PriceExcludingTax, PriceWithTax, numberAvailable, nbReview, imageUrl,
+                             productDescription, category])
+
+
+if response.ok:
+    soup = BS(response.content, features="html.parser")
+    links=[]
+    divlink = soup.find_all('div', {"class": "image_container"})
+    for div in divlink :
+        a = div.find('a')
+        link= a['href']
+        link=link[8:]
+        links.append(urlcatalogue+link)
+
+    for i in links :
+        livre(i,category,fichcsv)
+
--- livre.py
+++ livre.py
@@ -7,10 +7,11 @@ urlbase ='http://books.toscrape.com'
 url ='http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 category ='poetry'
 response = requests.get(url)
-
+print(url)
 if response.ok:
     soup = BS(response.content, features="html.parser")
-    title = soup.find('title')
+    title = soup.find('title').text
+    print(title)
     UPC = soup.find('th', string='UPC').next_sibling.text
     PriceWithTax= soup.find('th', string='Price (incl. tax)').next_sibling.text
     PriceExcludingTax = soup.find('th', string='Price (excl. tax)').next_sibling.text
@@ -23,6 +24,7 @@ if response.ok:
     imageUrl = imageUrl[6:]
     imageUrl = (urlbase,imageUrl)
     imageUrl = '/'.join(imageUrl)
+    print(url)
     with open('livre.csv', 'a', newline='') as fichiercsv:
         writer = csv.writer(fichiercsv)
         writer.writerow([url,UPC,title, PriceExcludingTax,PriceWithTax, numberAvailable, nbReview, imageUrl, productDescription,category])
